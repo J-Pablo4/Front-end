2c6fb94a374e4eba3ba190c6b9710567653bac1a
Juan Pablo <pablo.lopez@iteso.mx>
2022-11-24 15:06:09 -0600

    Generating post dinamically

--- assets/scripts/index_scripts.js
+++ assets/scripts/index_scripts.js
@@ -36,7 +36,62 @@ function init_form()
   });
 }
 
+function get_publications()
+{
+  axios.get('//localhost:3000/publications').then((respuesta) => {
+    const publications = respuesta.data;
+    const container = document.getElementById('publications');
+
+    console.log(respuesta.data);
+
+    publications.forEach(element => {
+      const user_name = element.user_name;
+      const place = element.place;
+      const description = element.description;
+      const photo = element.photo.split('/').pop();
+
+      // let photo = respuesta.photo.replace(/^.*[\\\/]/, '');
+      // console.log(photo)
+      // photo = photo.split(' ').pop();
+      const card = `
+      <div class="card shadow-lg mb-5" style="margin-top: 50px">
+      <div class="card-body d-flex">
+          <div class="d-flex">
+            <div class="profile-picture rounded-circle" id="post-general" style="margin-right: 20px"></div>
+            <a href="#" class="text-dark text-decoration-none fw-bold align-self-center">${user_name}</a>
+          </div>
+      </div>
+      <div style="margin-bottom: 15px">
+        <p class="fw-bold" style="margin-left: 85px; display: inline; font-size: 18px; margin-right: 5px">Place:</p>
+        <a href="#" class="text-dark text-decoration-none fw-bold" style="display: inline">${place}</a>
+      </div>
+      <img src="//localhost:3000/${photo}" alt="post" class="img-fluid" style="width: 900px;">
+      <div class="card-body">
+        <div class="d-flex">
+          <p>${description}</p>
+        </div>
+        <hr>
+        <p class="mb-0"><a href="#" class="text-dark fw-bold text-decoration-none">viajesdemontaña</a> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, minima.</p>
+        <p><a href="#" class="text-dark fw-bold text-decoration-none">casas_color_limon</a> Lorem ipsum, dolor sit amet consectetur</p>
+        <small class="d-block text-muted">12 HOURS AGO</small>
+      </div>
+      <form class="d-flex border-top py-3 px-2">
+        <input type="text" class="form-control border-0" placeholder="Comment...">
+        <button type="submit" class="btn new-post unauth">Publish</button>
+      </form>
+  </div>
+  `
+      container.innerHTML += card;
+    });
+    //Crear HTML
+  }).catch((err) => {
+    console.log('Hubo un error', err);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  get_publications();
+
   const fileInput = document.querySelector('input[type="file"]');
 
   fileInput.onchange = function(){
