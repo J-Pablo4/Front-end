bc46e87cdefcb4805e04db923a16c2002e18b4e4
Juan Pablo <pablo.lopez@iteso.mx>
2022-11-30 16:12:49 -0600

    aaaaaaaa

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('', express.static(__dirname + '/public'));
 
 app.listen( port, () => {
     console.log('App is running in port '+ port);
